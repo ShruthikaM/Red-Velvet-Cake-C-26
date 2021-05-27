67bb62749c40b2763459f6380b3fde01b5528743
ShruthikaM <73339795+ShruthikaM@users.noreply.github.com>
2021-05-27 14:35:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+/*Ingredients:
+
+Cake
+
+1/2 cup of unsalted butter (room temperature) (115g)
+1 1/2 cups of white granulated sugar (300g)
+2 Large eggs (add one at a time)
+2 Tbsp. unsweetened cocoa powder (24g)
+1 cup of buttermilk (250mL) (or use a tiny bit less then a cup plus
+1 Tbsp. of vinegar or lemon juice, let sit for 10 minutes)
+1 tsp. vanilla extract (5mL)
+1 tsp. of white vinegar (5mL)
+1/4 cup of oil (vegetable, etc.) (55g)
+2 to 2 1/2 Tbsp. of red food coloring liquid (30 to 40 mL)
+2 1/2 cups of all-purpose flour or cake flour (320g)
+1 tsp. of baking soda (5g)
+1 tsp. of salt (5g)
+Cream Cheese Frosting
+
+16 ounces of softened room temp cream cheese (450g)
+1 tsp. vanilla extract (5mL)
+1/2 cup unsalted butter, softened (room temperature) (115g)
+4 to 4 1/2 cups of confectioner's sugar (powdered sugar) (440 to 500g)
+Helpful Tools
+
+Bowls
+Hand mixer or stand mixer
+Spatula
+Sifter
+Whisk
+Measuring spoons
+9 inch cake pans
+Parchment paper (optional)
+Shortening or kitchen spray
+Flour or cocoa powder to dust the pans
+Wire racks
+Cake spatula
+Bread knife
+Cake pedestal (optional)
+Cake Icing smoother or cake comb (optional)
+Heart shaped cookie cutter (optional)
+The above links are affiliate links. As an Amazon Associate I earn from qualifying purchases.
+
+                                       STEPS
+    1)First preheat your oven to 350 F (176 C). Next let's make our cake batter.
+     Start by creaming your butter and your white granulated or caster sugar together in a large bowl.
+      Use a hand mixer or stand mixer to do this.
+
+    2)Now we add in our eggs one at a time. So drop one egg in, mix, once that egg is completely incorporated do the other egg.
+      Make sure to scrape the sides of the bowl as well with a spatula.   
+      
+    3)Now we add in our cocoa powder. It is a good idea to run it through some kind of a sifter. 
+    Cocoa powder tends to get a bit lumpy. Then mix again and scrape the sides as needed. 
+    Throughout this whole process we don't want to over mix, just until things are incorporated. 
+    If we mix too much we will add too much air into the batter which can cause air pockets in our cake. :) 
+
+    4)Now add in your vanilla extract to your buttermilk as well as your white vinegar.
+     If you don't have buttermilk you can make some by taking just shy of one cup of regular milk and adding one
+      tablespoon of lemon juice or vinegar and then let it sit out for about 10 to 15 minutes.
+     Stir in the vinegar and vanilla extract until combined.
+    
+    5)Now we add in our oil to the buttermilk mixture.
+     Next add in about 2 to 2 and 1/2 Tablespoons of red food coloring liquid. Then stir, until the mixture is a nice red color.
+     Set that mixture aside for now. 
+
+    6)Next we will add our salt and baking soda to our flour. You can use cake flour or all-purpose flour.
+    Cake flour tends to work a little bit better, but all-purpose works great too. Use a whisk to combine those ingredients.
+
+    7)Next we will add in about half of our food coloring buttermilk mixture and about half of our flour mixture, then turn on the mixture and mix until just combined.
+     Give the sides of the bowl a good scrape, then complete the process until all the ingredients have been mixed leaving you with a nice red velvet cake batter.
+
+    8)Now we just fill up two 9 inch or 8 inch cake pans with equal amounts of cake batter.
+     Make sure to use shortening or kitchen spray on the pans and then dust them with cocoa powder or flour and I always add a little 
+     round cut out of parchment paper on the bottom too as an added precaution, and my nonstick pans are great,
+      but I like to over-do it. lol Also it is a good idea to lightly tap the pans with the batter in it, this will
+       allow any cake bubbles to rise to the top, which helps to prevent air pockets, but sometimes it is unavoidable and you just get them,
+       a combination of over mixing, etc. But it won't effect the taste of the cake at all! So don't worry about it.
+      I never do.lol now bake those cakes for 23 to 30 minutes until when a toothpick comes out clean after poking it in the middle.
+
+    9)Now after the cakes come out of the oven let's leave them alone and allow them to cool in their pans for about 10 minutes. 
+    After that we can remove them from their pans, remove the parchment paper and then let the cakes cool completely on the rack. 
+    Afterwards you can place the cakes in the fridge and let them chill,
+     which helps with the frosting, or just go onto the the frosting step now, it's totally up to you! :)
+    
+                                                    ~~THE END~~ 
+    
+     */
+                                        
